6673910bf5b58ea17b4cb43f5d14aa4fb4e97950
Bromind <martin.vassor@hotmail.fr>
2014-02-26 22:11:00 +0100

    Add ObservableTravel

---
+++ src/travel/ObservableTravel.java
@@ -0,0 +1,197 @@
+package travel;
+
+import java.util.List;
+import travel.Observer;
+
+/**
+ * A wrapper to have an observable Travel.
+ * @author Bromind
+ */
+public class ObservableTravel extends Observable{
+  Travel travel;
+  List<Observer> observers = new LinkedList<Observer>();
+
+  /**
+   * @param empty True to construct an empty Travel
+   * @param o Initial Observers
+   */
+  public ObservableTravel(boolean empty, Observer... o){
+    travel = new Travel(empty);
+
+    for(int i = 0 ; i < o.length ; i ++){
+      subscribe(o[i]);
+    }
+  }
+
+  /**
+   * @param s Initial step
+   * @param o Initial Observers
+   */
+  public ObservableTravel(Step s, Observer... o){
+    travel = new Travel(s);
+    
+    for(int i = 0 ; i < o.length ; i ++){
+      subscribe(o[i]);
+    }
+  }
+
+  /**
+   * @param stepList Initial list of steps
+   * @param o Initial Observers
+   */
+  public ObservableTravel(List<Step> stepList, Observer... o){
+    travel = new Travel(stepList);
+
+    for(int i = 0 ; i < o.length ; i ++){
+      subscribe(o[i]);
+    }
+  }
+
+  /**
+   * @param file To import a ObservableTravel from an exported file.
+   * @param o Initial Observers
+   */
+  public ObservableTravel(String file, Observer... o){
+    travel = new Travel(file);
+
+    for(int i = 0 ; i < o.length ; i ++){
+      subscribe(o[i]);
+    }
+  }
+
+  /**
+   * To be notified when the ObservableTravel is updated
+   *
+   * @param o The Observer to notify
+   */
+  public void subscribe(Observer o){
+    observers.add(o);
+  }
+
+  /**
+   * Return the i-th Step of the travel
+   * @param i The index of the step.
+   */
+  public Step getStep(int i){
+    return travel.getStep(i);
+  }
+
+  /**
+   * Edit the travel and update Observers
+   */
+  public Travel editTravel(){
+    travel = travel.editTravel();
+    update();
+    return travel;
+  }
+
+  /**
+   * Edit a step
+   */
+  public Travel editTravelStep(){
+    travel = travel.editTravelStep();
+    update();
+    return travel;
+  }
+
+  /**
+   * Remove a step
+   *
+   * @param index The step to remove.
+   */
+  public Travel removeStep(int index){
+    travel.removeStep(index);
+    update();
+    return travel;
+  }
+
+  /**
+   * Add a step
+   *
+   * @param s The step to add.
+   */
+  public Travel addStep(Step s){
+    travel.addStep(s);
+    update();
+    return travel;
+  }
+
+  /**
+   * Write a LaTeX file
+   */
+  public void writeLaTeXFile(){
+    travel.writeLaTeXFile();
+  }
+
+  /**
+   * Write a LaTeX file
+   *
+   * @param fileDir The file name. 
+   */
+  public void writeLaTeXFile(String fileDir){
+    travel.writeLaTeXFile(String fileDir);
+  }
+      
+  /**
+   * Write a LaTeX file
+   *
+   * @param fileDir The file name.
+   * @param title The file title.
+   */
+  public void writeLaTeXFile(String fileDir, String title){
+    travel.writeLaTeXFile(String fileDir, String title);
+  }
+
+  /**
+   * Write a LaTeX file.
+   *
+   * @param fileDir The file name.
+   * @param title The file title.
+   * @param authorFamilyName The family name of the author.
+   */
+  public void writeLaTeXFile(String fileDir, String title, String authorFamilyName){
+	travel.writeLaTeXFile(String fileDir, String title, String authorFamilyName){
+  }
+
+  /**
+   * Write a LaTeX file.
+   *
+   * @param fileDir The file name.
+   * @param title The file title.
+   * @param authorFamilyName The family name of the author.
+   * @param authorFirstName The first name of the author.
+   */
+  public void writeLaTeXFile(String fileDir, String title, String authorFamilyName, String authorFirstName) {
+	travel.writeLaTeXFile(String fileDir, String title, String authorFamilyName, String authorFirstName);
+  }
+
+  /**
+   * Get the number of steps.
+   */
+  public int stepNumber(){
+    return travel.stepNumber();
+  }
+
+  /**
+   * @return A short string explaining the travel
+   */
+  public String shortName(){
+    return travel.shortName();
+  }
+
+  /**
+   * @return A string explaining the travel in details.
+   */
+  public String toString(){
+    return travel.toString();
+  }
+
+  /**
+   * Call update on each Observer
+   */
+  private void update(){
+    for(o: observers){
+      o.update();
+    }
+  }
+}
